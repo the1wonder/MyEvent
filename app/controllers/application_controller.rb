b05daa8840a913f4a7b63243101ce9e643daa842
the1wonder <the1wonder@hotmail.com>
2017-02-18 04:24:19 +0200

    Add logout and edit profile to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  helper_method :current_user ,:logged_in?
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
-
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  # helper_method :current_user ,:logged_in?
 
-    protected
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+  # def logged_in?
+  #   !!current_user
+  # end
 
-        def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :title, :company_name, :mobile,:website, :telephone, :email, :password) }
-            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :title, :company_name, :mobile, :website, :telephone, :email, :password, :current_password) }
-        end
-
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :title, :company_name, :mobile,:website, :telephone, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :title, :company_name, :mobile, :website, :telephone, :email, :password, :current_password) }
+  end
 end
